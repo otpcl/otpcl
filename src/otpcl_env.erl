13698dfadf3752746de29908fa4b2459c908e7e2
Ryan S. Northrup <northrup@yellowapple.us>
2019-01-04 20:07:45 -0800

    Take out the now-in-stdlib portions of otpcl_env

--- src/otpcl_env.erl
+++ src/otpcl_env.erl
@@ -1,35 +1,6 @@
 -module(otpcl_env).
 
--export([get_var/2, set_var/3, get_fun/2, set_fun/3, call_fun/3, import_fun/3,
-         default_state/0, default_funs/0, default_vars/0]).
-
-get_var(Name, {Funs, Vars}) ->
-    case maps:find(Name, Vars) of
-        {ok, Val} ->
-            {ok, Name, Val, {Funs, Vars}};
-        _ ->
-            % RFC: should we throw an error?  Or should we return a
-            % default?  We'll throw an error for now.
-            {error, no_such_var, {Funs, Vars}}
-    end.
-set_var(Name, Val, {Funs, Vars}) ->
-    {ok, Name, Val, {Funs, maps:put(Name, Val, Vars)}}.
-
-get_fun(Name, {Funs, Vars}) ->
-    case maps:find(Name, Funs) of
-        {ok, Fun} ->
-            {ok, Name, Fun, {Funs, Vars}};
-        _ ->
-            {error, no_such_fun, {Funs, Vars}}
-    end.
-set_fun(Name, Fun, {Funs, Vars}) ->
-    {ok, Name, Fun, {maps:put(Name, Fun, Funs), Vars}}.
-call_fun(Name, Args, State) ->
-    {ok, Name, Fun, State} = get_fun(Name, State),
-    apply(Fun, [Args, State]).
-import_fun(Module, Name, State) ->
-    WrappedFun = fun (Args, S) -> {apply(Module, Name, Args), S} end,
-    set_fun(Name, WrappedFun, State).
+-export([default_state/0, default_funs/0, default_vars/0]).
 
 default_state() ->
     {default_funs(), default_vars()}.
