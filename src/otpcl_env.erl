a47182d2cfc490f6dcf4bdb14b5a0180815d0bf3
Ryan S. Northrup <northrup@yellowapple.us>
2019-01-07 18:39:00 -0800

    Replace otpcl_env:default_state/0 with otpcl_stdlib calls
    This should make things a lot easier in the long run, after we start
    splitting out otpcl_stdlib into use-case-specific
    submodules (e.g. otpcl_metalib for manipulating functions,
    otpcl_strlib for manipulating strings, otpcl_mathlib for math
    operations, etc.)

--- src/otpcl_env.erl
+++ src/otpcl_env.erl
@@ -1,20 +1,9 @@
 -module(otpcl_env).
 
--export([default_state/0, default_funs/0, default_vars/0]).
+-export([default_state/0]).
 
 default_state() ->
-    {default_funs(), default_vars()}.
-default_funs() ->
-    %% Every OTPCL fun must accept a list of arguments and an input
-    %% state, and must return a return value and an output state.
-    #{ get    => fun otpcl_stdlib:get/2,
-       set    => fun otpcl_stdlib:set/2,
-       print  => fun otpcl_stdlib:print/2,
-       'if'   => fun otpcl_stdlib:'if'/2,
-       unless => fun otpcl_stdlib:unless/2,
-       incr   => fun otpcl_stdlib:incr/2,
-       decr   => fun otpcl_stdlib:decr/2,
-       import => fun otpcl_stdlib:import/2,
-       eval   => fun otpcl_stdlib:eval/2 }.
-default_vars() ->
-    #{'RETVAL' => ok}.
+    State0 = {#{}, #{}},
+    {{ok, _}, State1} = otpcl_stdlib:import([otpcl_stdlib], State0),
+    {ok, State2} = otpcl_stdlib:set(['RETVAL', ok], State1),
+    State2.
