91aff1cacd4c45fab0294f0f5f92339b8d461499
Ryan S. Northrup <northrup@yellowapple.us>
2019-01-07 18:31:52 -0800

    Add support for importing whole modules

--- src/otpcl_stdlib.erl
+++ src/otpcl_stdlib.erl
@@ -91,9 +91,39 @@ decr([By], State) ->
 decr([VarName, By], State) ->
     incr([VarName, By * -1], State).
 
+% Import the whole module...
+import([Module], State) ->
+    import([Module, otpcl_funs(Module:module_info(attributes))], State);
+% ...if it specifies Made For OTPCL™ functions
+import([Module, {otpcl_funs, Names}], State) ->
+    import([otpcl, Module, Names], State);
+% ...if it doesn't
+import([Module, no_otpcl_funs], State) ->
+    Names = [Name || {Name, _} <- Module:module_info(exports)],
+    import([erlang, Module, Names], State);
+% Import a list of functions from a module
+import([Type, Module, [Name|Names]], State) ->
+    {ok, NewState} = import([Type, Module, Name], State),
+    import([Type, Module, Names], NewState);
+import([otpcl, Module, []], State) ->
+    {{ok, Module}, State};
+% Import a specific function...
 import([Module, Name], State) ->
+    import([erlang, Module, Name], State);
+% ...if it's Made For OTPCL™
+import([otpcl, Module, Name], State) ->
+    funset([Name, fun Module:Name/2], State);
+% ...if it's a normal Erlang function (the default assumption)
+import([erlang, Module, Name], State) ->
     WrappedFun = fun (Args, S) -> {apply(Module, Name, Args), S} end,
     funset([Name, WrappedFun], State).
+
+otpcl_funs([{otpcl_funs, Names}|_]) ->
+    {otpcl_funs, Names};
+otpcl_funs([{_,_}|Rem]) ->
+    otpcl_funs(Rem);
+otpcl_funs([]) ->
+    no_otpcl_funs.
 
 eval([Name, Args], State) ->
     {Fun, State} = funget([Name], State),
