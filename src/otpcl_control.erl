c1e539b991592eed017b8b65e0c221c587658d87
Ryan S. Northrup ("RyNo") <northrup@yellowapple.us>
2021-04-04 06:28:53 -0700

    Implement `break` in `while` loops
    Also made this module stringy-aware, for good measure.

--- src/otpcl_control.erl
+++ src/otpcl_control.erl
@@ -1,18 +1,19 @@
 % @doc OTPCL flow control commands
 %
-% Following Tcl's tradition, OTPCL's flow control "structures" are essentially
-% just commands that take strings and evaluate them in various ways.  Said
-% commands' definitions live in this here module.
+% Following Tcl's tradition, OTPCL's flow control "structures" are
+% essentially just commands that take strings and evaluate them in
+% various ways.  Said commands' definitions live in this here module.
 -module(otpcl_control).
 
 -include("otpcl.hrl").
 
--export(['if'/2, unless/2, for/2, while/2, truthy/1, truthy/2]).
+-export(['if'/2, unless/2, for/2, while/2, truthy/1, truthy/2, break/2]).
 
--otpcl_cmds(['if', unless, for, while, truthy]).
+-otpcl_cmds(['if', unless, for, while, truthy, break]).
 
-% @doc Evaluates the second argument if the first is "truthy".  If there's a
-% third argument, it'll be evaluated if the first is not "truthy".
+% @doc Evaluates the second argument if the first is "truthy".  If
+% there's a third argument, it'll be evaluated if the first is not
+% "truthy".
 'if'([Test, Then], State) ->
     'if'([Test, Then, ""], State);
 'if'([Test, Then, Else], State) ->
@@ -23,41 +24,70 @@
             otpcl_eval:eval(Else, State)
     end.
 
-% @doc Inverse of `if'.  Does not accept a third clause, thus disallowing
-% "unless/else" constructs (if you want those, go back to Perl -- and I say this
-% as someone who loves Perl).
+% @doc Inverse of `if'.  Does not accept a third clause, thus
+% disallowing "unless/else" constructs (if you want those, go back to
+% Perl -- and I say this as someone who loves Perl).
 unless([Test, Then], State) ->
     'if'([Test, "", Then], State).
 
-% @doc Repeatedly evals the given clause with values from a list.  Synopsis:
+% @doc Repeatedly evals the given clause with values from a list.
+%
+% Synopsis:
 %
 % ```
 % for each in $list { mangle $each }
 % '''
 %
-% `each' here could be literally any variable name.  Return value will be the
-% value returned by the last invocation/evaluation of the body clause.
-for([Name, in, [Each|Rest], Do], State) ->
+% `each' here could be literally any variable name.  Return value will
+% be the value returned by the last invocation/evaluation of the body
+% clause.
+for([Name, in, Args, Do], State) ->
+    for([Name, <<"in">>, Args, Do], State);
+for([Name, <<"in">>, [Each|Rest], Do], State) ->
     {ok, NewState} = otpcl_meta:set([Name, Each], State),
     {_, NewerState} = otpcl_eval:eval([Do], NewState),
-    for([Name, in, Rest, Do], NewerState);
-for([_, in, [], _], State) ->
-    {RetVal, State} = otpcl_meta:get(['RETVAL'], State),
+    for([Name, <<"in">>, Rest, Do], NewerState);
+for([_, <<"in">>, [], _], State) ->
+    {RetVal, State} = otpcl_meta:get([<<"RETVAL">>], State),
     {RetVal, State}.
 
 % @doc Repeatedly evals the second clause for as long as the first clause
-% evaluates to a truthy value.  Word of caution: OTPCL currently lacks the
-% concept of a `break' statement, so if you ain't careful here you'll end up
-% with an infinitely-looping OTPCL interpreter (TODO: fix that).
+% evaluates to a truthy value.
+%
+% If you want to break out of the loop prematurely, use the `break'
+% command or set the `$BREAK' variable to any value (which is what
+% `break' does); `while' will pick this up, unset `$BREAK', and
+% immediately exit the loop.
 while([Pred, Do], State) ->
     {PredVal, NewState} = otpcl_eval:eval([Pred], State),
     case truthy(PredVal) of
         true ->
             {_, NewerState} = otpcl_eval:eval([Do], NewState),
-            while([Pred, Do], NewerState);
+            case should_break(NewerState) of
+                true ->
+                    {_, FinalState} = otpcl_meta:unset([<<"BREAK">>],
+                                                       NewerState),
+                    {RetVal, _} = otpcl_meta:get([<<"RETVAL">>], State),
+                    {RetVal, FinalState};
+                _ ->
+                    while([Pred, Do], NewerState)
+            end;
         false ->
-            {RetVal, State} = otpcl_meta:get(['RETVAL'], State),
+            {RetVal, _} = otpcl_meta:get([<<"RETVAL">>], State),
             {RetVal, State}
+    end.
+
+break(_, State) ->
+    {RetVal, _} = otpcl_meta:get([<<"RETVAL">>], State),
+    {_, NewState} = otpcl_meta:set([<<"BREAK">>, ok], State),
+    {RetVal, NewState}.
+
+should_break(State) ->
+    case otpcl_meta:get([<<"BREAK">>], State) of
+        {_, State} ->
+            true;
+        _ ->
+            false
     end.
 
 % @doc See `truthy/2'.
@@ -80,9 +110,11 @@ truthy(T) when is_tuple(T) andalso element(1, T) =:= error ->
 truthy(_) ->
     true.
 
-% @doc Determine if a value is "truthy".  All values are deemed to be "truthy"
-% (that is: will cause a control command's predicate to be treated as "true"
-% unless they are in any of the following categories:
+% @doc Determine if a value is "truthy".
+%
+% All values are deemed to be "truthy" (that is: will cause a control
+% command's predicate to be treated as "true") unless they are in any
+% of the following categories:
 %
 % <ul>
 % <li>An empty list, tuple, or binary</li>
